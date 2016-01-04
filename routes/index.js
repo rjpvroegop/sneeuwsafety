e4d284ada20d48e7ecb5112ec2a5c3aa54a52f2e
randy vroegop <s1079180@student.hsleiden.nl>
2016-01-04 22:31:45 +0100

    text geupdate en foto's verbeterd, Google Tracking

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homepage/template/index', { welcome: '../homepage/home/welcome.ejs' });
+  res.render('homepage/template/index', {url : req.protocol + '://' + req.get('host') + req.originalUrl});
 });
 
 module.exports = router;
